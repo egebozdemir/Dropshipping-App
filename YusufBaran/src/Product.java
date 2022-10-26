1f893a76c5561a61b47fe80a9794d33321606806
Kagan Aslancan <kagan.aslancan@gmail.com>
2022-10-26 20:42:15 +0300

    most purchase

---
+++ YusufBaran/src/Product.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Product {
+	
+	private String id;
+	private String title;
+	private double rate;
+	private int numberOfReviews;
+	private int price;
+	private double calcSalePrice = 0.0;
+
+	//Default constructor
+	public Product() {
+	}
+
+	//Full-arg constructor
+	public Product(String id, String title, String rate, String numberOfReviews, String price) {
+    this.id = id;
+    this.title = title;
+    this.rate = Double.parseDouble(rate);
+    this.numberOfReviews = Integer.parseInt(numberOfReviews);
+    this.price = Integer.parseInt(price);
+    this.calcSalePrice = this.price + ((this.rate/5.0)*100)*this.numberOfReviews;
+	}
+
+	//Accessor methods
+	public String getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public int getNumberOfReviews() {
+		return numberOfReviews;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+	public double getCalcSalePrice() {
+		return calcSalePrice;
+	}
+	
+	// Converts the string of product ID to the product object
+	public static Product parseProduct(String id) throws IOException {
+		Product product = null;
+		switch (id.charAt(0)) {
+		case 'E':
+			BufferedReader reader = FileIO.readCSV(DropshippingApp.PATH_TO_S1_PRODUCTS);
+			String line = reader.readLine();
+			while ((line = reader.readLine()) != null) {
+	    	StringTokenizer tokenizer = new StringTokenizer(line, ",");
+	    	String token = tokenizer.nextToken();
+	        if (token.equals(id)) {
+	        	product = new Product(token, tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+	        }
+	      }
+			break;
+		case 'C':
+			reader = FileIO.readCSV(DropshippingApp.PATH_TO_S2_PRODUCTS);
+			line = reader.readLine();
+			while ((line = reader.readLine()) != null) {
+	    	StringTokenizer tokenizer = new StringTokenizer(line, ",");
+	    	String token = tokenizer.nextToken();
+	        if (token.equals(id)) {
+	        	product = new Product(token, tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+	        }
+	      }
+			break;
+		case 'O':
+			reader = FileIO.readCSV(DropshippingApp.PATH_TO_S3_PRODUCTS);
+			line = reader.readLine();
+			while ((line = reader.readLine()) != null) {
+	    	StringTokenizer tokenizer = new StringTokenizer(line, ",");
+	    	String token = tokenizer.nextToken();
+	        if (token.equals(id)) {
+	        	product = new Product(token, tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());
+	        }
+	      }
+			break;
+		}
+		return product;
+	}
+
+
+	//To string override
+	@Override
+	public String toString() {
+
+		return String.format(" %s %s %s %s %s", id, title, rate, numberOfReviews, price);
+	}
+//		return "Product{" +
+//				"id='" + id + '\'' +
+//				", title='" + title + '\'' +
+//				", rate='" + rate + '\'' +
+//				", numberOfReviews='" + numberOfReviews + '\'' +
+//				", price='" + price + '\'' +
+//				'}';
+//	}
+
+
+}
---
+++ YusufBaran/src/SalesQuery.java
@@ -0,0 +1,117 @@
+import java.io.IOException;
+import java.util.Iterator;
+
+public class SalesQuery {
+
+    final static String PATH_TO_CUSTOMERS = "CENG211_Fall2022_HW1/Customers.csv";
+    final static String PATH_TO_S1_PRODUCTS = "CENG211_Fall2022_HW1/S1_Products.csv";
+    final static String PATH_TO_S1_SALES = "CENG211_Fall2022_HW1/S1_Sales.csv";
+    final static String PATH_TO_S2_PRODUCTS = "CENG211_Fall2022_HW1/S2_Products.csv";
+    final static String PATH_TO_S2_SALES = "CENG211_Fall2022_HW1/S2_Sales.csv";
+    final static String PATH_TO_S3_PRODUCTS = "CENG211_Fall2022_HW1/S3_Products.csv";
+    final static String PATH_TO_S3_SALES = "CENG211_Fall2022_HW1/S3_Sales.csv";
+
+    private Customer[] customerArray;
+    private Supplier S1Products;
+    private Supplier S2Products;
+    private Supplier S3Products;
+    private SalesManagement totalSales2D;
+
+
+    public SalesQuery() throws IOException {
+    	customerArray = new Customer[FileIO.csvFileLineNumberCalculator(FileIO.readCSV(PATH_TO_CUSTOMERS))];
+    	customerArray = FileIO.addCustomersToArray(PATH_TO_CUSTOMERS);
+        S1Products = new Supplier(FileIO.addProductsToArray(PATH_TO_S1_PRODUCTS));
+        S2Products = new Supplier(FileIO.addProductsToArray(PATH_TO_S2_PRODUCTS));
+        S3Products = new Supplier(FileIO.addProductsToArray(PATH_TO_S3_PRODUCTS));
+        totalSales2D = new SalesManagement(FileIO.addSalesToArray(PATH_TO_S1_SALES),FileIO.addSalesToArray(PATH_TO_S2_SALES),FileIO.addSalesToArray(PATH_TO_S3_SALES));
+    }
+
+
+    public SalesManagement getTotalSales2D() {
+        return totalSales2D;
+    }
+
+    public void getMostProfitableProduct(){
+        double temp_profit = 0;
+        Sales temp_sales = new Sales();
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
+                Sales curSales = totalSales2D.getSelectedSales(i,j);
+                if (curSales.getSalesPrice()- curSales.getProduct().getPrice() > temp_profit){
+                    temp_profit = curSales.getSalesPrice()- curSales.getProduct().getPrice();
+                    temp_sales = curSales;
+                }
+            }
+        }
+        System.out.println(temp_sales.getProduct().toString() + " --> "+  temp_profit + "TL");
+        }
+
+
+    public void getMostExpensiveProduct(){
+        double temp_salePrice = 0;
+        Sales temp_sales = new Sales();
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
+                Sales curSales = totalSales2D.getSelectedSales(i,j);
+                if (curSales.getSalesPrice()> temp_salePrice){
+                    temp_salePrice = curSales.getSalesPrice();
+                    temp_sales = curSales;
+                }
+            }
+        }
+        System.out.println(temp_sales.getProduct().toString() + " --> with sale price "+  temp_salePrice + "TL");
+    }
+
+    public void getCustomerWithMostPurchase() {
+    	int purchaseCount = 0;
+    	for (Customer customer : customerArray) {
+    		int temp = 0;
+				String customerName = customer.getId();
+				for (int i = 0; i < 3 ; i++) {
+          for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
+              Sales curSales = totalSales2D.getSelectedSales(i,j);
+              if (curSales.getCustomer().getId().equals(customerName)) {
+								temp++;
+							}
+          }
+				}
+				if (temp > purchaseCount) {
+					System.out.print(customerName + " : ");
+					purchaseCount = temp;
+				}
+			}
+    	System.out.println(purchaseCount);
+    }
+    
+    
+    public void getTotalProfit(){
+        double total_profit = 0;
+        for (int i = 0; i < 3 ; i++) {
+            for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
+                Sales curSales = totalSales2D.getSelectedSales(i,j);
+                double profit = curSales.getSalesPrice()- curSales.getProduct().getPrice();
+                total_profit += profit;
+            }
+        }
+        System.out.println(String.format("%f", total_profit) + "TL");
+    }
+
+    public void getLeastProfitableProduct(){
+        Sales temp_sales = totalSales2D.getSelectedSales(0,0);
+        double temp_profit = temp_sales.getSalesPrice()- temp_sales.getProduct().getPrice();
+            for (int j = 0; j < getTotalSales2D().getTotalSales()[0].length; j++) {
+                Sales curSales = totalSales2D.getSelectedSales(0,j);
+                if (curSales.getSalesPrice()- curSales.getProduct().getPrice() < temp_profit){
+                    temp_profit = curSales.getSalesPrice()- curSales.getProduct().getPrice();
+                    temp_sales = curSales;
+                }
+        }
+        System.out.println(temp_sales.getProduct().toString() + " --> "+  temp_profit + "TL");
+    }
+}
+
+
+
+
+
