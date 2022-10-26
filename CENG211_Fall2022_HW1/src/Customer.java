619c45bfde86f3445d2ad7b0cf3d0f0144878a21
egebozdemir <mege.bozdemir@gmail.com>
2022-10-27 02:45:48 +0300

    Build 1 deployed
    MVP that meets the HW requirements is built and deployed.

---
+++ CENG211_Fall2022_HW1/src/Customer.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class Customer {
+	
+	private String id;
+	private String name;
+	private String email;
+	private String country;
+	private String address;
+
+	//Default constructor
+	public Customer() {	
+	}
+
+	//Full-arg constructor
+	public Customer(String id, String name, String email, String country, String address) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.country = country;
+		this.address = address;
+	}
+
+	//Accessor methods
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+	
+	// Converts the string of customer ID to the customer object
+	public static Customer parseCustomer(String id) throws IOException {
+		Customer customer = null;
+		BufferedReader reader = FileIO.readCSV(DropshippingApp.PATH_TO_CUSTOMERS);
+		String line = reader.readLine();
+		while ((line = reader.readLine()) != null) {
+	    StringTokenizer tokenizer = new StringTokenizer(line, ",");
+	    String token = tokenizer.nextToken();
+	       if (token.equals(id)) {
+	        customer = new Customer(token, tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken(), tokenizer.nextToken());}
+		}
+		return customer;
+	}
+
+
+
+	//To string override
+	@Override
+	public String toString() {
+		return "Customer{" +
+				"id='" + id + '\'' +
+				", name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", country='" + country + '\'' +
+				", address='" + address + '\'' +
+				'}';
+	}
+
+
+
+
+
+}
