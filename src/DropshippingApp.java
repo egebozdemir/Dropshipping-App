b931273b366362eccc44c0205386b5caa067099a
esma irem tek <76255152+esmairemtek@users.noreply.github.com>
2022-10-27 20:30:16 +0300

    Deleted file path constants.
    Deleted file path constants to add them to FileIO class.

--- src/DropshippingApp.java
+++ src/DropshippingApp.java
@@ -2,14 +2,6 @@ import java.io.*;
 
 public class DropshippingApp {
 	
-	//File Path constants
-    final static String PATH_TO_CUSTOMERS = "CENG211_Fall2022_HW1/Customers.csv";
-    final static String PATH_TO_S1_PRODUCTS = "CENG211_Fall2022_HW1/S1_Products.csv";
-    final static String PATH_TO_S1_SALES = "CENG211_Fall2022_HW1/S1_Sales.csv";
-    final static String PATH_TO_S2_PRODUCTS = "CENG211_Fall2022_HW1/S2_Products.csv";
-    final static String PATH_TO_S2_SALES = "CENG211_Fall2022_HW1/S2_Sales.csv";
-    final static String PATH_TO_S3_PRODUCTS = "CENG211_Fall2022_HW1/S3_Products.csv";
-    final static String PATH_TO_S3_SALES = "CENG211_Fall2022_HW1/S3_Sales.csv";
 
     public static void main(String[] args) throws IOException {
     	
