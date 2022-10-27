ffb29ba6446a1c217d06d8a9eec4ddd306ccbd9f
esma irem tek <76255152+esmairemtek@users.noreply.github.com>
2022-10-27 21:11:14 +0300

    Update SalesQuery.java
    Changed some variable names, added some comments and deleted unused parts.

--- src/SalesQuery.java
+++ src/SalesQuery.java
@@ -13,118 +13,102 @@ public class SalesQuery {
 
     //private instance variables 
     private Customer[] customerArray;
-    private Supplier S1Products;
-    private Supplier S2Products;
-    private Supplier S3Products;
-    private SalesManagement totalSales2D;
+    private SalesManagement salesManagementObj;
 
     //default constructor
     public SalesQuery() throws IOException {
     	customerArray = new Customer[FileIO.csvFileLineNumberCalculator(FileIO.readCSV(PATH_TO_CUSTOMERS))];
     	customerArray = FileIO.addCustomersToArray(PATH_TO_CUSTOMERS);
-        S1Products = new Supplier(FileIO.addProductsToArray(PATH_TO_S1_PRODUCTS));
-        S2Products = new Supplier(FileIO.addProductsToArray(PATH_TO_S2_PRODUCTS));
-        S3Products = new Supplier(FileIO.addProductsToArray(PATH_TO_S3_PRODUCTS));
-        totalSales2D = new SalesManagement(FileIO.addSalesToArray(PATH_TO_S1_SALES),FileIO.addSalesToArray(PATH_TO_S2_SALES),FileIO.addSalesToArray(PATH_TO_S3_SALES));
+        salesManagementObj = new SalesManagement(FileIO.addSalesToArray(PATH_TO_S1_SALES),FileIO.addSalesToArray(PATH_TO_S2_SALES),FileIO.addSalesToArray(PATH_TO_S3_SALES));
     }
 
     //Accessor methods for evaluating the required queries given in the scope for HW
-    public SalesManagement getTotalSales2D() {
-        return totalSales2D;
+    public SalesManagement getSalesManagementObj() {
+        return salesManagementObj;
     }
 
-    public void getMostProfitableProduct(){ //For Query-1
+    public void getMostProfitableProduct(){ // For Query-1
         double temp_profit = 0;
-        Sales temp_sales = new Sales();
-        for (int i = 0; i < 3 ; i++) {
-            for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
-                Sales curSales = totalSales2D.getSelectedSales(i,j);
-                if (curSales.getSalesPrice()- curSales.getProduct().getPrice() > temp_profit){
-                    temp_profit = curSales.getSalesPrice()- curSales.getProduct().getPrice();
-                    temp_sales = curSales;
+        Sales temp_sale = new Sales();
+        for (int i = 0; i < 3 ; i++) { // i'th supplier
+            for (int j = 0; j < getSalesManagementObj().getTotalSalesArr()[i].length; j++) { // j'th sale
+                Sales currentSale = salesManagementObj.getSelectedSale(i,j);
+                if (currentSale.getSalesPrice() - currentSale.getProduct().getPrice() > temp_profit){
+                    temp_profit = currentSale.getSalesPrice() - currentSale.getProduct().getPrice();
+                    temp_sale = currentSale;
                 }
             }
         }
-        System.out.println(temp_sales.getProduct().toString() + " --> "+  temp_profit + " TL profit");
-    }
+	System.out.println("The most profitable product among the three suppliers: \n");
+        System.out.println(temp_sale.getProduct().toString() + " --> with profit of "+  temp_profit + " TL");
+     }
+   
 
     public void getMostExpensiveProduct(){ // For Query-2
         double temp_salePrice = 0;
-        Sales temp_sales = new Sales();
-        for (int i = 0; i < 3 ; i++) {
-            for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
-                Sales curSales = totalSales2D.getSelectedSales(i,j);
-                if (curSales.getSalesPrice()> temp_salePrice){
-                    temp_salePrice = curSales.getSalesPrice();
-                    temp_sales = curSales;
+        Sales temp_sale = new Sales();
+        for (int i = 0; i < 3 ; i++) { // i'th supplier
+            for (int j = 0; j < getSalesManagementObj().getTotalSalesArr()[i].length; j++) { // j'th sale
+                Sales current_sale = salesManagementObj.getSelectedSale(i,j);
+                if (current_sale.getSalesPrice() > temp_salePrice){
+                    temp_salePrice = current_sale.getSalesPrice();
+                    temp_sale = current_sale;
                 }
             }
         }
-        System.out.println(temp_sales.getProduct().toString() + " --> with sale price "+  temp_salePrice + " TL");
+        System.out.println("The most expensive product in terms of SalesPrice: \n");
+        System.out.println(temp_sale.getProduct().toString() + " --> with sale price "+  temp_salePrice + " TL");
     }
 
     public void getCustomerWithMostPurchase() {  // For Query-3
     	int purchaseCount = 0;
-    	Customer richCustomer = new Customer();
+    	Customer temp_customer = new Customer();
     	for (Customer customer : customerArray) {
     		int temp = 0;
-				String customerName = customer.getId();
-				for (int i = 0; i < 3 ; i++) {
-          for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
-              Sales curSales = totalSales2D.getSelectedSales(i,j);
-              if (curSales.getCustomer().getId().equals(customerName)) {
-								temp++;
-							}
-          }
+		String customerName = customer.getId();
+		for (int i = 0; i < 3 ; i++) { // i'th supplier
+          		for (int j = 0; j < getSalesManagementObj().getTotalSalesArr()[i].length; j++) { // j'th sale
+              			Sales currentSale = salesManagementObj.getSelectedSale(i,j);
+				if (currentSale.getCustomer().getId().equals(customerName)) {
+					temp++;
 				}
-				if (temp > purchaseCount) {
-					richCustomer = customer;
-					purchaseCount = temp;
-				}
-			}
-    	System.out.println(richCustomer.getId() + " " + richCustomer.getName() + " " + richCustomer.getEmail() +  " " + richCustomer.getCountry() + " " + richCustomer.getAddress() + " -> " + purchaseCount + " purchases");
-    }
+          		}
+		}
+		if (temp > purchaseCount) {
+			temp_customer = customer;
+			purchaseCount = temp;
+		}
+	}
+    	System.out.println("The customer who purchases the most products for all three suppliers: \n");
+    	System.out.println(temp_customer.toString() + " --> with " + purchaseCount + " purchases");    }
     
     public void getTotalProfit(){ // For Query-4
-    	//int purchaseCount = 0;
         double total_profit = 0;
-        for (int i = 0; i < 3 ; i++) {
-            for (int j = 0; j < getTotalSales2D().getTotalSales()[i].length; j++) {
-                Sales curSales = totalSales2D.getSelectedSales(i,j);
-                double profit = curSales.getSalesPrice()- curSales.getProduct().getPrice();
+        for (int i = 0; i < 3 ; i++) { // i'th supplier
+ for (int j = 0; j < getSalesManagementObj().getTotalSalesArr()[i].length; j++) { // j'th sale
+ 	Sales currentSale = salesManagementObj.getSelectedSale(i,j);
+                double profit = currentSale.getSalesPrice()- currentSale.getProduct().getPrice();
                 total_profit += profit;
             }
         }
+        System.out.println("The total profit that is made from all sales: \n");
         System.out.println(String.format("%.2f", total_profit) + " TL");
     }
 
     public void getLeastProfitableProduct(){  // For Query-5
-    	//int purchaseCount = 0;
-        Sales temp_sales = totalSales2D.getSelectedSales(0,0);
-        double temp_profit = temp_sales.getSalesPrice()- temp_sales.getProduct().getPrice();
-            for (int j = 0; j < getTotalSales2D().getTotalSales()[0].length; j++) {
-                Sales curSales = totalSales2D.getSelectedSales(0,j);
-                if (curSales.getSalesPrice()- curSales.getProduct().getPrice() < temp_profit){
-                    temp_profit = curSales.getSalesPrice()- curSales.getProduct().getPrice();
-                    temp_sales = curSales;
+	Sales temp_sale = salesManagementObj.getSelectedSale(0,0);
+        double temp_profit = temp_sale.getSalesPrice()- temp_sale.getProduct().getPrice();
+        for (int j = 0; j < getSalesManagementObj().getTotalSalesArr()[0].length; j++) { // j'th sale of S1
+                Sales currentSale = salesManagementObj.getSelectedSale(0,j);
+                if (currentSale.getSalesPrice()- currentSale.getProduct().getPrice() < temp_profit){
+                    temp_profit = currentSale.getSalesPrice()- currentSale.getProduct().getPrice();
+                    temp_sale = currentSale;
                 }
         }
-        System.out.println(temp_sales.getProduct().toString() + " --> "+  temp_profit + " TL profit");
+	System.out.println("The least-profit product of S1: \n");
+        System.out.println(temp_sale.getProduct().toString() + " --> with profit of "+  temp_profit + " TL");
+
     }
-
-	public Supplier getS1Products() {
-		return S1Products;
-	}
-
-	public Supplier getS2Products() {
-		return S2Products;
-	}
-
-	public Supplier getS3Products() {
-		return S3Products;
-	}
-    
-    
     
 }
 
